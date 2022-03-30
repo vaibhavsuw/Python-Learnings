4ec315dd7654662c339d9f9e389f00711f66a8d9
vaibhavsuw <102162376+vaibhavsuw@users.noreply.github.com>
2022-03-30 07:48:47 +0530

    Update Pizza Order System.py

--- Pizza Order System.py
+++ Pizza Order System.py
@@ -7,11 +7,11 @@ if(size == 'S'):
   bill = 15
   if(add_pepperoni == 'Y'):
     bill += 2
-if(size == 'M'):
+elif(size == 'M'):
   bill = 20
   if(add_pepperoni == 'Y'):
     bill += 3
-if(size == 'L'):
+elif(size == 'L'):
   bill = 25
   if(add_pepperoni == 'Y'):
     bill += 3
@@ -19,8 +19,3 @@ if(extra_cheese=='Y'):
   bill += 1
   
 print(f"Your final bill is: ${bill}.")
-
-
-
-
-
