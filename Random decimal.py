6e8184e9ec415c7225dab22875c5f7ce7c9ce013
vaibhavsuw <102162376+vaibhavsuw@users.noreply.github.com>
2022-03-31 22:59:59 +0530

    Create Random decimal.py

---
+++ Random decimal.py
@@ -0,0 +1,10 @@
+import random
+
+random_int = random.randint(1,100)
+print(random_int)
+
+random_float = random.random()
+print(random_float)
+
+random_decimal = random_int*random_float
+print(random_decimal)
